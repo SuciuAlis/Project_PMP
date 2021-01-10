2a6198f86de0f33b4ad98c5d9d6d0dfe3f13b1e6
SuciuAlis <61494544+SuciuAlis@users.noreply.github.com>
2021-01-10 14:24:27 +0200

    memoryGame

--- project.ino
+++ project.ino
@@ -18,11 +18,12 @@ byte colPins[COLS] = {9, 8, 7, 6}; //conectam coloanele keypadului la pini
 // declararea keypadului 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-int maxDigits = 5;		// numarul de cifre retinute pentru castigarea jocului
+int minDigits = 3;		// numarul de cifre cu care se incepe
 int score = 0;			// scorul care se va afisa jucatorului
 int cifre[8]={};		// vectorul folosit pentru retinerea cifrelor
-int wrong = 0;			
+int wrong = 0, wrong2 = 0;			
 int i=0,j=0,k=0;
+int level = 1;			// nivelul de inceput
 
 void setup(){
   Serial.begin(9600);
@@ -38,68 +39,103 @@ void loop(){
   if (key == '*'){   // start game daca se apasa tasta corespunzatoare caracterului *
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Digit:");   // se afiseaza cifra si scorul
+    lcd.print("Digit:");   // se afiseaza cifra, scorul si nivelul
     lcd.setCursor(0, 1);
     lcd.print("Score:");
     lcd.print(score);
-    i=0;
-    while(i<maxDigits){
-        cifre[i]=random(0,10); // se adauga cifre random de la 0 la 10 
-        lcd.setCursor(6, 0);    // se seteaza cursorul la lcd pentru a putea afisa fiecare cifra noua
-        lcd.print(cifre[i]);   // afisarea cifrei
-        delay(500);
-        j=0;
-        while(j<=i){  // cu j se vor parcurge cifrele din vectorul de cifre
-            char keyNum = keypad.getKey();
-            while(!keyNum){   // se asteapta citirea caracterului din keypad
-                keyNum = keypad.getKey(); 
-            }
-            if(toCifra(keyNum) == cifre[j]){ 
-              // daca caracterul citit din keypad corespunde cu elementul curent din vectorul de cifre
-              // atunci se trece la urmatorul element din vectorul de cifre
-                //Serial.println(numere[j]);
+    lcd.setCursor(9, 1);
+    lcd.print("Level:");
+    lcd.print(level);
+	level = 1;
+    wrong2 = 0;
+	
+    while(level<5){		
+		i=0;
+    	while(i<minDigits+level){		// la fiecare nivel se incrementeaza numarul de cifre necesare
+        	cifre[i]=random(0,10); // se adauga cifre random de la 0 la 10 
+        	lcd.setCursor(6, 0);    // se seteaza cursorul la lcd pentru a putea afisa fiecare cifra noua
+        	lcd.print(cifre[i]);   // afisarea cifrei
+        	j=0;
+        	while(j<=i){  // cu j se vor parcurge cifrele din vectorul de cifre
+            	char keyNum = keypad.getKey();
+            	while(!keyNum){   // se asteapta citirea caracterului din keypad
+                	keyNum = keypad.getKey(); 
+            	}
+            	if(toCifra(keyNum) == cifre[j]){ 
+              	// daca caracterul citit din keypad corespunde cu elementul curent din vectorul de cifre
+              	// atunci se trece la urmatorul element din vectorul de cifre
+                //Serial.println(cifre[j]);
                 //Serial.println(keyNum);
                 //Serial.println("**");
-                j++;
-            }
-            else{   //daca se greseste, atunci jocul e pierdut
-                wrong = 1;
-                break;
-            }
-        }
-        if (wrong == 0){  // daca nu s-a gresit cifra se continua cu un numar nou
-            score++;
-            lcd.setCursor(6, 1);
-            lcd.print(score);
-            i++;
-        }
-        else if (wrong == 1){ // daca s-a gresit cifra jocul este pierdut 
-            lcd.clear();
-            lcd.print("Wrong digit"); // se printeaza mesaj in cazul in care s-a gresit cifra
-            lcd.setCursor(0, 1);
-            lcd.print("Game over");
-            score = 0;    // se reseteaza scorul si var wrong
-            wrong = 0;
-            delay(1000);
-            lcd.clear();
-            lcd.print("Press * to");  // se printeaza mesaj in cazul in care se doreste reinceperea unui alt joc 
-            lcd.setCursor(0, 1);
-            lcd.print("restart");
-            break;
-        }
+                	j++;
+            	}
+            	else{   //daca se greseste, atunci jocul e pierdut
+                	wrong = 1;
+                	break;
+            	}
+        	}
+        	if (wrong == 0){  // daca nu s-a gresit cifra se continua cu un numar nou
+            	score++;
+            	lcd.setCursor(6, 1);
+            	lcd.print(score);
+            	i++;
+        	}
+        	else if (wrong == 1){ // daca s-a gresit cifra jocul este pierdut 
+            	lcd.clear();
+            	lcd.print("Wrong digit"); // se printeaza mesaj in cazul in care s-a gresit cifra
+            	lcd.setCursor(0, 1);
+            	lcd.print("Game over");
+            	wrong = 0;		// se reseteaza var wrong
+              	wrong2 = 1;
+            	delay(1000);
+            	lcd.clear();
+            	lcd.print("Press * to");  // se printeaza mesaj in cazul in care se doreste reinceperea unui alt joc 
+            	lcd.setCursor(0, 1);
+            	lcd.print("restart");
+            	break;
+        	}
+    	}
+		if (wrong2 == 1){	
+			score = 0;
+          	wrong2 = 0;
+          	level = 1;
+			break;
+		}
+    	if(i==minDigits+level){			// daca s-au tastat toate cifrele nivelului corect, se incremeteaza nivelul, se afiseaza si se trece mai departe
+      		level++;		//incrementam nivelul
+          	//Serial.println("LEVEL");
+            //Serial.println(level);
+         	lcd.clear();
+          	lcd.setCursor(0, 0);
+          	lcd.print("Next level");
+          	delay(500);
+          	lcd.clear();
+    		lcd.setCursor(0, 0);
+    		lcd.print("Digit:");   // se afiseaza cifra, scorul si nivelul
+    		lcd.setCursor(0, 1);
+    		lcd.print("Score:");
+    		lcd.print(score);
+    		lcd.setCursor(9, 1);
+    		lcd.print("Level:");
+    		lcd.print(level);
+        	wrong = 0;
+    	}
+  	
+    }
+   
+    if (level == 5){		// daca se ajunge la nivelul 5, jocul este castigat
+    	lcd.clear();
+        lcd.print("You WIN"); 
+      	lcd.setCursor(0, 1);
+       	lcd.print("* to restart");
+      	level = 1;
+      	score = 0;
+      	wrong = 0;
+      	wrong2 = 0;
+      	delay(500);
     }
     
-    if(i==maxDigits){
-        lcd.clear();
-        lcd.print("You WIN");   // se printeaza mesaj in caz de castig joc
-        lcd.setCursor(0, 1);
-        lcd.print("* to restart");
-        score = 0;    // se reseteaza scorul si var wrong in caz de castig, pentru urmatorul joc
-        wrong = 0;
-        delay(500);
-    }
   }
-  
 }
 int toCifra(char c){    // functie pentru identificarea caracterului si returnarea cifrei corespunzatoare
   if(c=='1')return 1;
